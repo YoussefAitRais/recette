9efec756e914f3e7186cabc039fc9d8e892041b3
Youssef Ait Rais <copybuisness007@gmail.com>
2025-04-29 10:32:39 +0200

    navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { RecipeComponent } from "./recipe/recipe.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, RecipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
